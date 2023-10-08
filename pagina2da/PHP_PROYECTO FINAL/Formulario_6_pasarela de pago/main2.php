e6e50d53899c9d1775905f6c8e5c731272c1a021
Sofia15M <sofiaortizacevedo2007@gmail.com>
2023-10-07 21:03:07 -0500

    PHP - NO funcional

---
+++ pagina2da/PHP_PROYECTO FINAL/Formulario_6_pasarela de pago/main2.php
@@ -0,0 +1,30 @@
+<?php
+    echo "<pre>";
+    print_r ( $_POST);
+
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+    $c = $_POST["c"];
+    $d = $_POST['d'];    
+    $e = $_POST['e'];
+    $f = $_POST['f'];
+    $g = $_POST['g'];   
+    $h = $_POST['h'];  
+    $i = $_POST['i'];
+    $j = $_POST['j'];
+    $k = $_POST['k'];
+    $l = $_POST['l'];
+    $m = $_POST['m'];
+    $n = $_POST['n'];
+    $o = $_POST['o'];
+    $p = $_POST['p'];
+    $q = $_POST['q'];
+    $r = $_POST['r'];
+    $s = $_POST['s'];
+    $t = $_POST['t'];
+    $u = $_POST['u'];
+    $v = $_POST['v'];
+?>
+ 
+
+  
